ac73613a28157cd048c1347b536f51e01609c7cd
Marcin Lawniczak <marcin@lawniczak.me>
2018-11-13 20:28:13 +0100

    Fix JobOffer model factory/seeder

--- database/factories/JobOfferFactory.php
+++ database/factories/JobOfferFactory.php
@@ -6,14 +6,14 @@ $factory->define(App\JobOffer::class, function (Faker $faker) {
 
     return [
         'name'          => $faker->name,
-        'description'   => $faker->text,
-        'salary'        => $faker->randomFloat(2),
+        'description'   => $faker->text(50),
+        'salary'        => $faker->numberBetween(0,100),
         'start_date'    => $faker->date(),
         'end_date'      => $faker->date(),
-        'area_id'       => $faker->randomNumber(),
-        'position_id'   => $faker->randomNumber(),
-        'degree_id'     => $faker->randomNumber(),
-        'address_id'    => $faker->randomNumber(),
+        'area_id'       => $faker->numberBetween(0,100),
+        'position_id'   => $faker->numberBetween(0,100),
+        'degree_id'     => $faker->numberBetween(0,100),
+        'address_id'    => $faker->numberBetween(0,100),
         'created_at'    => $faker->dateTimeBetween('-6 months', 'now'),
         'updated_at'    => $faker->dateTimeBetween('-3 months', 'now')
     ];
--- database/migrations/2018_10_03_040710_create_job_offers_table.php
+++ database/migrations/2018_10_03_040710_create_job_offers_table.php
@@ -24,7 +24,6 @@ class CreateJobOffersTable extends Migration
             $table->integer('position_id');
             $table->integer('degree_id');
             $table->integer('address_id');
-            $table->integer('company_id');
             $table->softDeletes();
             $table->timestamps();
         });
